f0b7bdb693f62ddcf06ecd2f04b9e809433743bf
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-09-01 15:26:13 +0200

    Into backend

---
+++ back/src/main/java/org/factoriaf5/comicbooks/ComicbooksApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.comicbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicbooksApplication.class, args);
+	}
+
+}
